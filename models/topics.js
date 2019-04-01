68a94a5d8017dad20e024e2e7a4e369526352617
Northcoders4 <northcoders4@Northcoders4s-MacBook-Pro.local>
2019-04-01 19:09:49 +0100

    get route for topics added

--- models/topics.js
+++ models/topics.js
@@ -1,5 +1,5 @@
 const connection = require('../db/connection');
 
 exports.getTopics = () => {
-  return connection.select('*').from('shops');
+  return connection.select('*').from('topics');
 };
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,13 @@
 const apiRouter = require('express').Router();
 const { methodNotAllowed } = require('../errors');
 const { getToApi } = require('../controllers/api');
-const { topicsRouter } = require('../routes/topics');
+const topicsRouter = require('../routes/topics');
 
 apiRouter
   .route('/')
   .get(getToApi)
-  .all(methodNotAllowed)
-  .use('/topics', topicsRouter);
+  .all(methodNotAllowed);
+
+apiRouter.use('/topics', topicsRouter);
 
 module.exports = apiRouter;
