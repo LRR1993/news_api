0bc5c725e86c4418063a921ee1a134c4b1a0cf16
Northcoders4 <northcoders4@Northcoders4s-MacBook-Pro.local>
2019-04-04 09:29:23 +0100

    knexfile pushed to heroku

---
+++ knexfile.js
@@ -0,0 +1,29 @@
+const ENV = process.env.NODE_ENV || 'development';
+
+const baseConfig = {
+  client: 'pg',
+  migrations: {
+    directory: './db/migrations',
+  },
+  seeds: {
+    directory: './db/seeds',
+  },
+};
+
+const customConfigs = {
+  development: {
+    connection: {
+      database: 'news_api',
+    },
+  },
+  test: {
+    connection: {
+      database: 'news_api_test',
+    },
+  },
+  production: {
+    connection: process.env.DATABASE_URL,
+  },
+};
+
+module.exports = { ...baseConfig, ...customConfigs[ENV] };
