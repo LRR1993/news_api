134743022272d979978fc52eb8fb49edb0b24360
Northcoders4 <northcoders4@Northcoders4s-MacBook-Pro.local>
2019-04-04 10:10:02 +0100

    updates to package json

--- knexfile.js
+++ knexfile.js
@@ -3,27 +3,27 @@ const ENV = process.env.NODE_ENV || 'development';
 const baseConfig = {
   client: 'pg',
   migrations: {
-    directory: './db/migrations',
+    directory: './db/migrations'
   },
   seeds: {
-    directory: './db/seeds',
-  },
+    directory: './db/seeds'
+  }
 };
 
 const customConfigs = {
   development: {
     connection: {
-      database: 'news_api',
-    },
+      database: 'news_api'
+    }
   },
   test: {
     connection: {
-      database: 'news_api_test',
-    },
+      database: 'news_api_test'
+    }
   },
   production: {
-    connection: process.env.DATABASE_URL,
-  },
+    connection: process.env.DATABASE_URL
+  }
 };
 
 module.exports = { ...baseConfig, ...customConfigs[ENV] };
---
+++ spec/utils.spec.js
@@ -0,0 +1,307 @@
+const { expect } = require('chai');
+const {
+  timestampFormat,
+  authorFormat,
+  commentRef,
+  commentFormat,
+  patchFormat
+} = require('../utils/utils');
+
+describe('Utils', () => {
+  describe('#timestampFormat', () => {
+    it('doesnt mutate the original information', () => {
+      const input = [];
+      expect(timestampFormat(input)).to.not.equal(input);
+    });
+    it('changes javascript timestamp into an sql time stamp', () => {
+      const input = [
+        {
+          title: 'Living in the shadow of a great man',
+          topic: 'mitch',
+          author: 'butter_bridge',
+          body: 'I find this existence challenging',
+          created_at: 1542284514171,
+          votes: 100
+        }
+      ];
+      const output = [
+        {
+          title: 'Living in the shadow of a great man',
+          topic: 'mitch',
+          author: 'butter_bridge',
+          body: 'I find this existence challenging',
+          created_at: '2018-11-15T12:21:54.171Z',
+          votes: 100
+        }
+      ];
+      expect(timestampFormat(input)).to.eql(output);
+    });
+    it('changes javascript timestamp array into an sql time stamp', () => {
+      const input = [
+        {
+          title: 'Living in the shadow of a great man',
+          topic: 'mitch',
+          author: 'butter_bridge',
+          body: 'I find this existence challenging',
+          created_at: 1542284514171,
+          votes: 100
+        },
+        {
+          title: 'Eight pug gifs that remind me of mitch',
+          topic: 'mitch',
+          author: 'icellusedkars',
+          body: 'some gifs',
+          created_at: 1289996514171
+        }
+      ];
+      const output = [
+        {
+          title: 'Living in the shadow of a great man',
+          topic: 'mitch',
+          author: 'butter_bridge',
+          body: 'I find this existence challenging',
+          created_at: '2018-11-15T12:21:54.171Z',
+          votes: 100
+        },
+        {
+          title: 'Eight pug gifs that remind me of mitch',
+          topic: 'mitch',
+          author: 'icellusedkars',
+          body: 'some gifs',
+          created_at: '2010-11-17T12:21:54.171Z'
+        }
+      ];
+      expect(timestampFormat(input)).to.eql(output);
+    });
+  });
+  describe('#authorFormat', () => {
+    it('doesnt mutate the original information', () => {
+      const input = [];
+      expect(authorFormat(input)).to.not.equal(input);
+    });
+    it('returns comments with author as key', () => {
+      const input = [
+        {
+          body:
+            "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+          belongs_to: "They're not exactly dogs, are they?",
+          created_by: 'butter_bridge',
+          votes: 16,
+          created_at: 1511354163389
+        }
+      ];
+      const output = [
+        {
+          body:
+            "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+          belongs_to: "They're not exactly dogs, are they?",
+          author: 'butter_bridge',
+          votes: 16,
+          created_at: 1511354163389
+        }
+      ];
+      expect(authorFormat(input)).to.eql(output);
+    });
+    it('returns new array of comments with the created_by replaced with the correct key (author)', () => {
+      const input = [
+        {
+          body:
+            "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+          belongs_to: "They're not exactly dogs, are they?",
+          created_by: 'butter_bridge',
+          votes: 16,
+          created_at: 1511354163389
+        },
+        {
+          body:
+            'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+          belongs_to: 'Living in the shadow of a great man',
+          created_by: 'butter_bridge',
+          votes: 14,
+          created_at: 1479818163389
+        },
+        {
+          body:
+            'Replacing the quiet elegance of the dark suit and tie with the casual indifference of these muted earth tones is a form of fashion suicide, but, uh, call me crazy — onyou it works.',
+          belongs_to: 'Living in the shadow of a great man',
+          created_by: 'icellusedkars',
+          votes: 100,
+          created_at: 1448282163389
+        }
+      ];
+      const output = [
+        {
+          body:
+            "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+          belongs_to: "They're not exactly dogs, are they?",
+          author: 'butter_bridge',
+          votes: 16,
+          created_at: 1511354163389
+        },
+        {
+          body:
+            'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+          belongs_to: 'Living in the shadow of a great man',
+          author: 'butter_bridge',
+          votes: 14,
+          created_at: 1479818163389
+        },
+        {
+          body:
+            'Replacing the quiet elegance of the dark suit and tie with the casual indifference of these muted earth tones is a form of fashion suicide, but, uh, call me crazy — onyou it works.',
+          belongs_to: 'Living in the shadow of a great man',
+          author: 'icellusedkars',
+          votes: 100,
+          created_at: 1448282163389
+        }
+      ];
+      expect(authorFormat(input)).to.eql(output);
+    });
+  });
+  describe('#commentRef', () => {
+    it('doesnt mutate the original information', () => {
+      const input = [];
+      expect(commentRef(input)).to.not.equal(input);
+    });
+    it('returns a lookup object when given an array of a single comment', () => {
+      const input = [
+        {
+          article_id: 1,
+          title: 'Living in the shadow of a great man',
+          topic: 'mitch',
+          author: 'butter_bridge',
+          body: 'I find this existence challenging',
+          created_at: 1542284514171,
+          votes: 100
+        }
+      ];
+      const output = {
+        'Living in the shadow of a great man': 1
+      };
+      expect(commentRef(input)).to.eql(output);
+    });
+    it('returns a lookup object when given an array of multiple commemts', () => {
+      const input = [
+        {
+          article_id: 1,
+          title: 'Living in the shadow of a great man',
+          topic: 'mitch',
+          author: 'butter_bridge',
+          body: 'I find this existence challenging',
+          created_at: 1542284514171,
+          votes: 100
+        },
+        {
+          article_id: 2,
+          title: 'Eight pug gifs that remind me of mitch',
+          topic: 'mitch',
+          author: 'icellusedkars',
+          body: 'some gifs',
+          created_at: 1289996514171
+        },
+        {
+          article_id: 3,
+          title: 'Student SUES Mitch!',
+          topic: 'mitch',
+          author: 'rogersop',
+          body:
+            'We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages',
+          created_at: 1163852514171
+        }
+      ];
+      const output = {
+        'Living in the shadow of a great man': 1,
+        'Eight pug gifs that remind me of mitch': 2,
+        'Student SUES Mitch!': 3
+      };
+      expect(commentRef(input)).to.eql(output);
+    });
+  });
+  describe('#commentFormat', () => {
+    it('returns correct format for comments schema for an array length of 1', () => {
+      const lookup = {
+        "They're not exactly dogs, are they?": 1
+      };
+      const data = [
+        {
+          body:
+            "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+          belongs_to: "They're not exactly dogs, are they?",
+          created_by: 'butter_bridge',
+          votes: 16,
+          created_at: 1511354163389
+        }
+      ];
+      const output = [
+        {
+          body:
+            "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+          article_id: 1,
+          created_by: 'butter_bridge',
+          votes: 16,
+          created_at: 1511354163389
+        }
+      ];
+      expect(commentFormat(lookup, data)).to.eql(output);
+    });
+    it('returns correct format for shop schema for an array', () => {
+      const lookup = {
+        "They're not exactly dogs, are they?": 1,
+        'Living in the shadow of a great man': 2
+      };
+      const output = [
+        {
+          body:
+            "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+          article_id: 1,
+          created_by: 'butter_bridge',
+          votes: 16,
+          created_at: 1511354163389
+        },
+        {
+          body:
+            'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+          article_id: 2,
+          created_by: 'butter_bridge',
+          votes: 14,
+          created_at: 1479818163389
+        },
+        {
+          body:
+            'Replacing the quiet elegance of the dark suit and tie with the casual indifference of these muted earth tones is a form of fashion suicide, but, uh, call me crazy — onyou it works.',
+          article_id: 2,
+          created_by: 'icellusedkars',
+          votes: 100,
+          created_at: 1448282163389
+        }
+      ];
+      const data = [
+        {
+          body:
+            "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+          belongs_to: "They're not exactly dogs, are they?",
+          created_by: 'butter_bridge',
+          votes: 16,
+          created_at: 1511354163389
+        },
+        {
+          body:
+            'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+          belongs_to: 'Living in the shadow of a great man',
+          created_by: 'butter_bridge',
+          votes: 14,
+          created_at: 1479818163389
+        },
+        {
+          body:
+            'Replacing the quiet elegance of the dark suit and tie with the casual indifference of these muted earth tones is a form of fashion suicide, but, uh, call me crazy — onyou it works.',
+          belongs_to: 'Living in the shadow of a great man',
+          created_by: 'icellusedkars',
+          votes: 100,
+          created_at: 1448282163389
+        }
+      ];
+      expect(commentFormat(lookup, data)).to.eql(output);
+    });
+  });
+});
